2cbe87248d3a3be4b685efe6ec0ef54ebaad097a
giovanni <giovanni.boscarino@xenit.eu>
2017-01-09 14:47:20 +0100

    fixed link to pdfjs library after porting the wrapper project to dyn ext

--- src/docpreviewPdfjs.ts
+++ src/docpreviewPdfjs.ts
@@ -2,7 +2,8 @@ import { DOM as _, createElement as __, ReactElement } from 'react';
 
 export function  DocPreviewPdfJs ({src} : {src: string}) : ReactElement<any> {
     // the iframe is loaded with PdfJs library which is embedded in the jar which contains the web app (bundle.js)
-    const pdsJsWrapperSrc = "/alfresco/finder/pdfjs/index.html?file=" + src; 
+    // example link to pdfjs lib: http://localhost:8080/alfresco/s/finder/resources/static/pdfjs/index.html (cfr project https://bitbucket.org/xenit/finder-ethias-de-amp)
+    const pdsJsWrapperSrc = "/alfresco/s/finder/resources/static/pdfjs/index.html?file=" + src; 
     return _.iframe({className: 'pdfjs-container-iframe', border: "0", src: pdsJsWrapperSrc}); 
 }
 
