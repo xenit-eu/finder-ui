6be1e1f92ad170987ace219394f3ce1b50c22e64
Jasper Hilven <jasperhilven@gmail.com>
2017-03-02 10:48:25 +0100

    Minor fixes

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@ export { DocumentTreeNode, DocumentTreeNode_t, DocumentTree } from './treeview';
 export { MenuItem_t, DocList_t, DocList, SortDirection_t, Column_t, Row_t } from './doclist';
 export { SearchBox_t, SearchBox, Term_t } from './searchbox';
 export { MetaDataDialog_t, MetaDataDialog } from './metadataDialog';
-export { Metadata_t, MetadataType_t  } from './metadata';
+export { Metadata_t, MetadataType_t } from './metadata';
 export { MetaDataPanel_t, MetaDataPanel } from './metadataPanel';
 export { Facet_t, Facets_t, Facets } from './facets';
 export { DocPreview } from './docpreview';
 export { DocPreviewPdfJs } from './docpreviewPdfjs';
-export { PageMenu, PageMenu_t } from './pageMenu'
+export { PageMenu, PageMenu_t } from './pageMenu';
+export { paneltab, TabsPanelOnChange_t, TabsPanel } from './tabspanel'
