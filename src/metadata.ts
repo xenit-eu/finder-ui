8dbc24923cdd4f71db61fa57d77f102240a3a9e1
Jasper Hilven <jasperhilven@gmail.com>
2017-03-14 14:24:05 +0100

    Metadatapanel multi groups

--- src/metadata.ts
+++ src/metadata.ts
@@ -4,7 +4,9 @@ import DatePicker from 'material-ui/DatePicker';
 import AutoComplete from 'material-ui/AutoComplete';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
-
+import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import { MetaDataPanel_Group, MetadataPanel_GroupInfo } from './metadataPanel'
 
 export enum MetadataType_t {
     STRING,
@@ -25,18 +27,18 @@ export type Metadata_t = {
 // SelectField wrapper component to be able to change the current value 
 // when selecting a new item from the drop-down list.
 class SelectionField extends Component<any, any> {
-    state = {value: this.props.field.value};
-    render () {
+    state = { value: this.props.field.value };
+    render() {
         return __(SelectField, {
             floatingLabelText: this.props.field.label,
             value: this.state.value,
             disabled: this.props.disable,
-            onChange: (event: any, key: number, payload: any) => { 
-                this.props.field.value = payload; 
-                this.setState({value: payload}) 
+            onChange: (event: any, key: number, payload: any) => {
+                this.props.field.value = payload;
+                this.setState({ value: payload })
             }
-        }, 
-        this.props.field.options.map((a : any) => __(MenuItem, {value: a.value, primaryText: a.text}))   
+        },
+            this.props.field.options.map((a: any) => __(MenuItem, { value: a.value, primaryText: a.text }))
         );
     }
 }
@@ -46,40 +48,68 @@ class SelectionField extends Component<any, any> {
 function metadataFilter(a: Metadata_t) {
     return !/\}(store\-protocol|node\-dbid|content|locale|store\-identifier|lastThumbnailModification|node\-uuid)$/.test(a.name);
 }
+function RenderField(field: Metadata_t, editable: boolean){
+    let disable = field.disable;
+    if (!editable) {
+        disable = true;
+    }
+    switch (field.type) {
+        case MetadataType_t.DATE:
+            return _.span({ className: 'metadata-field' }, __(DatePicker, {
+                hintText: "Portrait Inline Dialog",
+                container: "inline",
+                disabled: disable,
+                floatingLabelText: field.label,
+                autoOk: true,
+                onChange: (empty: any, newDate: Date) => field.value = newDate.toISOString(),
+                defaultDate: new Date(field.value),
+            }));
 
-export function metadataFields (fields: Metadata_t[], editable : boolean = true) {
-    return fields.filter(metadataFilter).map((field) => {
-        let disable = field.disable;
-        if (!editable) {
-            disable = true;
-        }
-        switch(field.type) {
-            case MetadataType_t.DATE:
-                return _.span({className: 'metadata-field'}, __(DatePicker, {
-                    hintText: "Portrait Inline Dialog", 
-                    container: "inline",
-                    disabled: disable,
-                    floatingLabelText: field.label,
-                    autoOk: true,
-                    onChange: (empty : any, newDate: Date) => field.value = newDate.toISOString(),
-                    defaultDate: new Date(field.value),
-                }));
+        case MetadataType_t.LIST:
+            return _.span({ className: 'metadata-field select' }, __(SelectionField, {
+                field: field,
+                disable: disable
+            }));
 
-            case MetadataType_t.LIST:
-                 return _.span({className: 'metadata-field select'}, __(SelectionField, {
-                        field: field,
-                        disable: disable
-                    }));
+        case MetadataType_t.STRING:
+            return _.span({ className: 'metadata-field' }, __(TextField, {
+                key: field.name + field.value,
+                hintText: "Type value...",
+                onChange: (evt: any) => field.value = evt.target.value,
+                disabled: disable,
+                floatingLabelText: field.label,
+                defaultValue: field.value,
+            }));
+    }
+    throw "Did not return properly";
+}
+const defaultGroup = {
+    label: "Default",
+    id: "default",
+    expanded: true,
+    order: 1000
+};
 
-            case MetadataType_t.STRING:
-                return _.span({className: 'metadata-field'}, __(TextField, {
-                    key: field.name+field.value,
-                    hintText: "Type value...",
-                    onChange: (evt: any) => field.value = evt.target.value,
-                    disabled: disable,
-                    floatingLabelText: field.label,
-                    defaultValue: field.value,
-                    }));
-        }
+function GetfieldsInGroups(fields: Metadata_t[],groupInfo: MetadataPanel_GroupInfo){
+ let groupsWithChildren: { [id: string]: { group: MetaDataPanel_Group, items: Metadata_t[] } } =
+        { "default": { group: defaultGroup, items: [] } };
+    groupInfo.Groups.forEach(g => groupsWithChildren[g.id] = ({ group: g, items: [] }));
+    fields.forEach(f => groupsWithChildren[groupInfo.ItemToGroup[f.name] ? groupInfo.ItemToGroup[f.name] : "default"].items.push(f));
+    let groupsWithChildrenList = Object.keys(groupsWithChildren).map(id => groupsWithChildren[id]);
+    groupsWithChildrenList.sort((a, b) => a.group.order - b.group.order);
+    return groupsWithChildrenList;
+}
+
+export function metadataFields(fields: Metadata_t[], editable: boolean = true, groupInfo: MetadataPanel_GroupInfo | undefined = undefined) {
+    const filteredFields = fields.filter(metadataFilter);
+    if (!groupInfo)
+        return filteredFields.map(f => RenderField(f, editable));
+    let groupsWithChildrenList = GetfieldsInGroups(fields,groupInfo);
+    return groupsWithChildrenList.map(g => {
+        const expandable = g.group.id != "default";
+        const childItems = g.items.map(f => __(CardText, { expandable: expandable }, _.div({},RenderField(f, editable))));
+        const header = __(CardHeader, { title: g.group.label, actAsExpander: expandable, showExpandableButton: expandable });
+        const items = [header,...childItems];
+        return __(Card, {}, items);
     });
 }
--- src/metadataPanel.ts
+++ src/metadataPanel.ts
@@ -22,17 +22,28 @@ export type MetaDataPanel_t = {
     editionMode: boolean,
     fields: Metadata_t[],
     onEdit: () => void,
-    onSave: (fields: Metadata_t[]) => void
+    onSave: (fields: Metadata_t[]) => void,
+    groups?: MetadataPanel_GroupInfo
 };
+export type MetaDataPanel_Group = {
+    label: string,
+    id: string,
+    expanded: boolean,
+    order: number
+};
+export type MetadataPanel_GroupInfo = {
+    Groups: MetaDataPanel_Group[],
+    ItemToGroup: { [id: string]: string };
+}
 
-export function MetaDataPanel({allowEdition, editionMode, fields, onEdit, onSave}: MetaDataPanel_t, style? : Metadatapanel_style) {
+export function MetaDataPanel({allowEdition, editionMode, fields, onEdit, onSave, groups}: MetaDataPanel_t, style?: Metadatapanel_style) {
     return _.div({ className: 'metadata' }, [
-        allowEdition ? _.div({className: 'metadata-header'}, [
-            editionMode 
-                ? __(FlatButton, { label: "Save", primary: true, keyboardFocused: true, onTouchTap: () => onSave(fields) }) 
+        allowEdition ? _.div({ className: 'metadata-header' }, [
+            editionMode
+                ? __(FlatButton, { label: "Save", primary: true, keyboardFocused: true, onTouchTap: () => onSave(fields) })
                 : __(FlatButton, { label: "Edit", primary: true, keyboardFocused: true, onTouchTap: () => onEdit() })
-        ]) : _.div({className: 'metadata-header'}),
-        _.div({className: 'metadata-content' + (editionMode ? ' edited' : ''), style: style && style.ContentStyle ? style.ContentStyle : {}}, metadataFields(fields, editionMode))
+        ]) : _.div({ className: 'metadata-header' }),
+        _.div({ className: 'metadata-content' + (editionMode ? ' edited' : ''), style: style && style.ContentStyle ? style.ContentStyle : {} }, metadataFields(fields, editionMode, groups))
     ]
     );
 
