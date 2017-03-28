aec412fb0eb84f90eb3b5fe9f27e45c60d1eacef
Jasper Hilven <jasperhilven@gmail.com>
2017-03-28 12:26:43 +0200

    Extend doclist

--- src/doclist.ts
+++ src/doclist.ts
@@ -1,13 +1,15 @@
 import { createElement as __, DOM as _, ReactElement } from "react";
-import { Pager, Pager_t } from "./pager";
-
-import FontIcon from "material-ui/FontIcon";
-import IconButton from "material-ui/IconButton";
-import IconMenu from "material-ui/IconMenu";
-import MenuItem from "material-ui/MenuItem";
-import MoreHorizIcon from "material-ui/svg-icons/navigation/more-horiz";
-
+import { Pager, Pager_t } from './pager'
+import Checkbox from 'material-ui/Checkbox'
+import FontIcon from 'material-ui/FontIcon';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import MoreHorizIcon from 'material-ui/svg-icons/navigation/more-horiz';
+import FlatButton from 'material-ui/FlatButton'
+import FileDownload from 'material-ui/svg-icons/file/file-download';
 import "./doclist.less";
+
 
 type OnMenuSelected = (rowIndex: number, menuIndex: number, key?: string) => void;
 
@@ -34,7 +36,7 @@ function RowMenu({rowIndex, menuItems, onMenuSelected}: RowMenu_t): ReactElement
     );
 }
 
-export type Row_t = { [k: string]: string };
+export type Row_t = { props: { [k: string]: string }, toggled?: boolean, noderef: string };
 
 export enum SortDirection_t {
     NONE,
@@ -60,10 +62,13 @@ export type DocList_t = {
     rowMenu: MenuItem_t[],
     onPageSelected: (pageIndex: number) => void,
     onRowSelected: (rowIndex: number) => void,
+    togglable: boolean,
+    onRowToggled: (checked: boolean, i: number, row: Row_t) => void,
     onMenuSelected: OnMenuSelected,
     onSortColumnSelected: OnSortColumnSelected_t,
     className: string,
-    rowStyle: (i: number) => any,
+    onDownloadButtonClick:()=>void,
+    rowStyle: (i: number) => any
 };
 
 function sortIcon(c: Column_t, onSortColumnSelected: OnSortColumnSelected_t): ReactElement<any> | undefined {
@@ -92,17 +97,20 @@ function sortIcon(c: Column_t, onSortColumnSelected: OnSortColumnSelected_t): Re
         : undefined;
 }
 
-export function DocList({columns, data, pager, onPageSelected, rowMenu, onRowSelected, onMenuSelected, onSortColumnSelected, className, rowStyle}: DocList_t): ReactElement<any> {
-    const header = _.thead({ key: "header" }, [
-            _.tr({ key: "head" }, [_.th({ key: "Menu" }, ""), ...columns.map((c) => _.th({ key: c.name + c.label }, [sortIcon(c, onSortColumnSelected), c.label]))]),
-        ]);
+
+export function DocList({columns, data, pager, onPageSelected, rowMenu, onRowSelected,onDownloadButtonClick, onMenuSelected, onSortColumnSelected, togglable, onRowToggled, className, rowStyle}: DocList_t): ReactElement<any> {
+    const headerelements = [_.th({ key: 'Menu' }, '')]
+        .concat(togglable ? [_.th({ key: 'toggle' }, __(FlatButton, { icon: __(FileDownload, {onClick:onDownloadButtonClick}) }))] : [])
+        .concat(columns.map(c => _.th({ key: c.name + c.label }, [sortIcon(c, onSortColumnSelected), c.label])));
+    const header = _.thead({ key: 'header' }, [_.tr({ key: 'head' }, headerelements)]);
     const style = rowStyle ? rowStyle : (i: number) => ({});
-    const bodycontent = data.map((row, i) => _.tr({ style: style(i), key: i }, [
-        _.td({ key: "_menu" }, __(RowMenu, { rowIndex: i, menuItems: rowMenu, onMenuSelected })),
-        ...columns.map((col) => _.td({ key: col.name + col.label, onClick: () => onRowSelected(i) }, col.format ? col.format(row[col.name], row) : row[col.name])),
-    ]));
-    const body = _.tbody({ key: "body" }, bodycontent);
-    const tableProps = { key: "table", className: className || "table table-hover table-striped table-mc-purple table-condensed" };
+    const singleRowElements = (row: Row_t, i: number) =>
+        [_.td({ key: '_menu' }, __(RowMenu, { rowIndex: i, menuItems: rowMenu, onMenuSelected: onMenuSelected }))]
+            .concat((togglable ? [_.td({ key: 'toggle' }, __(Checkbox, { checked: row.toggled, onCheck: (ev: any, checked: boolean) => onRowToggled(checked, i, row) }))] : []))
+            .concat(columns.map(col => _.td({ key: col.name + col.label, onClick: () => onRowSelected(i) }, col.format ? col.format(row.props[col.name], row) : row.props[col.name])));
+    const bodycontent = data.map((row, i) => _.tr({ style: style(i), key: i }, singleRowElements(row, i)));
+    const body = _.tbody({ key: 'body' }, bodycontent);
+    const tableProps = { key: "table", className: className || 'table table-hover table-striped table-mc-purple table-condensed' };
     const table = _.table(tableProps, [header, body]); // table
     const pagerComponent = __(Pager, { totalItems: pager.totalItems, pageSize: pager.pageSize, selected: pager.selected, pageSelected: onPageSelected });
     return _.div({ className: "doclist" }, pager.totalItems > 0 ? [pagerComponent, table] : []);
