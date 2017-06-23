f100767b307450885894970a75866ae3fd76c5b8
giovanni <giovanni.boscarino@xenit.eu>
2017-06-23 15:09:51 +0200

    XENFIN-314 save/restore column sets

--- src/columnspicker.ts
+++ src/columnspicker.ts
@@ -4,6 +4,9 @@ import FontIcon from "material-ui/FontIcon";
 import IconButton from "material-ui/IconButton";
 import { Component, createElement as __ } from "react";
 
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+
 declare var require: any;
 // tslint:disable-next-line:no-var-requires
 const Sortable: any = require("react-sortablejs");
@@ -13,10 +16,6 @@ import "./columnspicker.less";
 export type Column_t = {
     name: string,
     label: string,
-    /*alignRight?: boolean,
-    sortable?: boolean,
-    sortDirection?: SortDirection_t,
-    format?: (a: any, props: Row_t) => string,*/
 };
 
 export type ColumnsPicker_t = {
@@ -36,7 +35,30 @@ const sortableOptions = {
     },
 };
 
-export class ColumnsPicker extends Component<ColumnsPicker_t, any> {
+const saveButtonStyle = {
+    position: "relative",
+    top: -30,
+    left: 20,
+
+};
+
+type ColumnSet_t = {
+    label: string,
+    columns: string[], // list of column names.
+};
+
+
+type State_t = {
+    opened: boolean, // open dialog.
+    selected: string[], // list of labels
+    sets: ColumnSet_t[], 
+    selectedSet: string, // selected ColumnSet label.
+};
+
+const storageKey = "users-column-sets";
+
+
+export class ColumnsPicker extends Component<ColumnsPicker_t, State_t> {
 
     private mappingByName: {[k: string]: Column_t};
     private mappingByLabel: {[k: string]: Column_t};
@@ -49,27 +71,81 @@ export class ColumnsPicker extends Component<ColumnsPicker_t, any> {
 
         this.state = {
             opened: false,
-            all: this.props.allColumns.filter(a => this.props.selectedColumns.indexOf(a.name) === -1).map(c => c.label),
             selected: this.props.selectedColumns.map(a => this.mappingByName[a].label),
+            sets: JSON.parse(localStorage.getItem(storageKey) || "[]"),
+            selectedSet: "",
         };
     }
 
     private handleDone () {
+        console.log(this.state.selected.map((c: string) => this.mappingByLabel[c].label).join("\n"));
         this.props.onDone(this.state.selected.map((c: string) => this.mappingByLabel[c].name));
-        this.setState({opened: false});
+        this.setState({ opened: false} as State_t);
     }
 
     private handleShowDialog () {
-        this.setState({opened: true});
+        this.setState({opened: true} as State_t);
     }
+
+    private handleSave () {
+        const set = this.state.sets.filter(s => s.label === this.state.selectedSet)[0];
+        set.columns = this.state.selected.map(l => this.mappingByLabel[l].name  );
+        localStorage.setItem(storageKey, JSON.stringify(this.state.sets));
+    }
+
+    private handleSaveAsNew () {
+        const name : string = prompt("Column set name") || "DEFAULT";
+        let list = this.state.sets;
+
+        list.push({label: name, columns: this.state.selected.map(l => this.mappingByLabel[l].name  )})
+        localStorage.setItem(storageKey, JSON.stringify(list));
+
+        this.setState({sets: list, selectedSet: name} as State_t);
+    }
+
+    private handleChangeSet (event: any, index: number, value: string) {
+        const set = this.state.sets.filter(s => s.label === value)[0];
+        this.setState({
+            selectedSet: value,
+            selected: set.columns.map(a => this.mappingByName[a].label),
+        } as State_t);
+    }
+
+    private handleDelete() {
+        const newSets = this.state.sets.filter(s => s.label !== this.state.selectedSet);
+        const selectedSet = newSets.length > 0 ? newSets[0] : null;
+        this.setState({
+            sets: newSets,
+            selected: selectedSet ?  selectedSet.columns.map(a => this.mappingByName[a].label) : [],
+            selectedSet: selectedSet ? selectedSet.label : "",
+        } as State_t);
+        localStorage.setItem(storageKey, JSON.stringify(newSets));
+    }
+
+
+    private handleChangeTargetSortable(items: string[]) {
+        console.log("--- handleChangeTarget ---");
+        console.log(items.join("\n"))
+        this.setState( {selected: items } as State_t )
+    }
+
+    private handleChangeSourceSortable(items: string[]) {
+        console.log("--- handleChangeSource ---");
+        console.log(items.join("\n"))
+
+        const selected = this.props.allColumns.map(c => c.label).filter(a => items.indexOf(a) === -1);
+        this.setState( {selected: selected } as State_t )
+    }
+
 
     public render() {
 
-        const all = this.state.all.map((val: string, key: string) => __("li", {key, "data-id": val}, val));
-        const selected = this.state.selected.map((val: string, key: string) => __("li", {key, "data-id": val}, val));
+        const selected = this.state.selected.map((val: string) => __("li", {key: val, "data-id": val}, val));
+        const others = this.props.allColumns.filter(a => this.state.selected.indexOf(a.label) === -1).map(c => c.label).map((val: string) => __("li", {key: val, "data-id": val}, val));
 
         const dialogButtons = [
             __(FlatButton, {
+                key: "buttonDone",
                 label: "Done",
                 primary: true,
                 keyboardFocused: false,
@@ -79,6 +155,7 @@ export class ColumnsPicker extends Component<ColumnsPicker_t, any> {
         ];
 
         const dialog = __(Dialog, {
+            key: "dialog",
             title: "Columns to display",
             actions: dialogButtons,
             modal: true,
@@ -87,30 +164,41 @@ export class ColumnsPicker extends Component<ColumnsPicker_t, any> {
             bodyClassName: "columns-picker-content",
             autoScrollBodyContent: true,
         }, [
-            __("h3", {}, "Displayed columns"),
-            __("div", {} ),
+
+            __("h3", {key: "hdr-1"}, "Saved column sets"),
+
+            __(SelectField, {key: "sf", value: this.state.selectedSet, onChange: this.handleChangeSet.bind(this)}, this.state.sets.map(o => __(MenuItem, {value: o.label, primaryText: o.label}) )),
+            __(FlatButton, {key: "bs", style: saveButtonStyle, label: "Save", onClick: this.handleSave.bind(this)}),
+            __(FlatButton, {key: "bsa", style: saveButtonStyle, label: "Save as new...", onClick: this.handleSaveAsNew.bind(this)}),
+            __(FlatButton, {key: "bd", style: saveButtonStyle, label: "Delete", onClick: this.handleDelete.bind(this)}),
+
+            __("hr", {key: "hr-1"}),   
+
+            __("h3", {key: "hdr-2"}, "Columns to display"),
+            __("div", {key: "selected"} ),
                 __(Sortable, {
                     options: sortableOptions,
                     className: "block-list-target",
-                    onChange: (items) => this.setState({ selected: items }),
+                    onChange: this.handleChangeTargetSortable.bind(this),
                     tag: "ul",
                 }, selected),
-            __("hr"),
-            __("h3", {}, "Other available columns"),
-            __("div", {style: {marginBottom: 40}},
+            __("hr", {key: "hr-2"}),
+            __("h3", {key: "hdr-3"}, "Other available columns"),
+            __("div", {key: "other", style: {marginBottom: 40}},
                 __(Sortable, {
                     options: sortableOptions,
                     className: "block-list-source",
-                    onChange: (items) => this.setState({ all: items }),
+                    onChange: this.handleChangeSourceSortable.bind(this),
                     tag: "ul",
-                }, all),
+                }, others),
               ),
-            __("p", {}, "Drag and drop the name on the above section to display it."),
+            __("p", {key: "footer"}, "Drag and drop the name on the above section to display it."),
             ],
         );
 
         return __("div", {className: "columns-picker"}, [
             this.props.visible ? __(IconButton, {
+                key: "button",
                 keyboardFocused: false,
                 onTouchTap: this.handleShowDialog.bind(this),
                 onClick: this.handleShowDialog.bind(this),
