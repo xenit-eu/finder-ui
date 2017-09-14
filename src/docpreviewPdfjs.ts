104cde31c9e9bf66c0fcbd6c88ddec564de3a016
Jasper Hilven <jasperhilven@gmail.com>
2017-09-14 15:46:28 +0200

    DocPreviewPdfJs encode uri

--- src/docpreviewPdfjs.ts
+++ src/docpreviewPdfjs.ts
@@ -1,13 +1,9 @@
 import { createElement as __, DOM as _, ReactElement } from "react";
-//@Component DocPreviewPdfJs
-//@ComponentDescription "Preview of a document (using the PDFJS library) Note: To use this component, the pdfjs library should be accessible at the following URL:  /alfresco/finder/pdfjs "
-//@Method DocPreview Returns ReactComponent
-//@MethodDescription "DocPreview({param1: value1, param2: value2, ...})"
-//@Param src string "url of the document to be displayed"
 
-export function DocPreviewPdfJs ({src}: {src: string}): ReactElement<any> {
-    // the iframe is loaded with PdfJs library which is embedded in the jar which contains the web app (bundle.js)
-    // example link to pdfjs lib: http://localhost:8080/alfresco/s/finder/resources/static/pdfjs/index.html (cfr project https://bitbucket.org/xenit/finder-ethias-de-amp)
-    const pdsJsWrapperSrc = "/alfresco/s/finder/resources/static/pdfjs/index.html?file=" + src;
-    return _.iframe({className: "pdfjs-container-iframe", border: "0", src: pdsJsWrapperSrc});
+export function DocPreviewPdfJs({src}: { src: string }): ReactElement<any> {
+    if (!src || src.length == 0){
+        return _.div({ className: "PDFJS_Without_file" });
+    }
+    const pdsJsWrapperSrc = "/alfresco/s/finder/resources/static/pdfjs/index.html?file=" + encodeURIComponent(src);
+    return _.iframe({ className: "pdfjs-container-iframe", border: "0", src: pdsJsWrapperSrc });
 }
