5ff9ff19ce4647f46f2ad5383b19ab89f29a9b62
Jasper Hilven <jasperhilven@gmail.com>
2017-09-21 11:25:27 +0200

    Add Theming file

---
+++ src/AlfredTheming.ts
@@ -0,0 +1,24 @@
+export const APIColorPalette = {
+    monoChrome0: "#231F20",
+    monoChrome1: "#58595B",
+    monoChrome2: "#808285",
+    monoChrome3: "#A7A9AC",
+    color0: "#33A89F",
+    color1: "#24B99D",
+    color2: "#50BD90",
+    color3: "#67C18C",
+    color4: "#3EB549",
+    color5: "#8FCA79",
+    color6: "#C1DA63",
+    color7: "#D7DF23",
+};
+export const FinderColorPalette = {
+    monoChrome0: "#231F20",
+    monoChrome1: "#58595B",
+    monoChrome2: "#808285",
+    monoChrome3: "#A7A9AC",
+    color0: "#09A89E",
+    color1: "#5BAAA2",
+    color2: "#7CCCBF",
+    color3: "#97D5C9",
+};
