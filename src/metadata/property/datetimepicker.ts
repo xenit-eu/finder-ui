7d085f46f110f7e1e213e847dce2142267b574c9
Lars Vierbergen <lars.vierbergen@xenit.eu>
2017-12-12 15:15:31 +0100

    [XENFIN-509] Add renderer for date and datetime fields

---
+++ src/metadata/property/datetimepicker.ts
@@ -0,0 +1,43 @@
+import debug from "debug";
+import DatePicker from "material-ui/DatePicker";
+import { Component, createElement as __, DOM as _, FormEvent, ReactElement } from "react";
+const d = debug("finder-ui:metadata:property:datetimepicker");
+
+import { FieldSkeleton_Props_t, RenderMode } from "../fields";
+import { PropertyRenderConfig_t, PropertyRenderer_t } from "./interface";
+
+const DateTimePicker: PropertyRenderer_t<Date> = (config: PropertyRenderConfig_t<Date>) => {
+    return (props: FieldSkeleton_Props_t) => {
+        const defaultValueString = config.parameters["override-default-value"] || null;
+        let defaultValue = undefined;
+        if(defaultValueString) {
+            if(defaultValueString === "today") {
+                defaultValue = new Date();
+            } else {
+                try {
+                    defaultValue = new Date(Date.parse(defaultValueString));
+                } catch(e) {
+                    d("Can not parse default value %s to date: %O", defaultValueString, e);
+                }
+            }
+        }
+
+        if (props.renderMode !== RenderMode.VIEW) {
+            return _.span({ className: "metadata-field" }, __(DatePicker, {
+                fullWidth: true,
+                autoOk: true,
+                container: "inline",
+                hintText: "Pick a date",
+                onChange: (evt: FormEvent<{}>, value: Date) => {
+                    props.onChange(config.mapToModel(props.node, value));
+                },
+                defaultDate: defaultValue,
+                value: config.mapToView(props.node),
+            }));
+        } else {
+            const viewValue = config.mapToView(props.node);
+            return _.span({ className: "metadata-value" }, viewValue ? viewValue.toString() : "");
+        }
+    };
+};
+export default DateTimePicker;
--- src/metadata/property/index.ts
+++ src/metadata/property/index.ts
@@ -2,3 +2,4 @@ export { default as TextBox } from "./textbox";
 export { default as Label } from "./label";
 export { default as TextArea } from "./textarea";
 export { default as CheckBox } from "./checkbox";
+export { default as DateTimePicker } from "./datetimepicker";
