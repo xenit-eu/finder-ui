0cc514d33631f85816b1ec83e1d8aafeacd25622
Lars Vierbergen <lars.vierbergen@xenit.eu>
2017-12-13 14:09:21 +0100

    [XENFIN-509] Add renderer for list constraints

---
+++ src/metadata/property/listconstraint.ts
@@ -0,0 +1,60 @@
+import { MenuItem, SelectField } from "material-ui";
+import { Component, createElement as __, DOM as _, FormEvent, ReactElement } from "react";
+
+import { FieldSkeleton_Props_t, RenderMode } from "../fields";
+import { PropertyRenderConfig_t, PropertyRenderer_t } from "./interface";
+
+type ListConstraint_State_t = {
+    menuItems: Array<{
+        key: string,
+        value: string,
+    }>,
+    loaded: boolean,
+};
+
+const ListConstraint: PropertyRenderer_t<string> = (config: PropertyRenderConfig_t<string>) => {
+    class ListConstraintInner extends Component<FieldSkeleton_Props_t, ListConstraint_State_t> {
+        constructor(props: FieldSkeleton_Props_t) {
+            super(props);
+            this.state = {
+                menuItems: [],
+                loaded: false,
+            };
+        }
+
+        public componentDidMount() {
+            config.parameters.resolver.lookup([])
+                .then(items => this.setState({ menuItems: items, loaded: true }));
+        }
+
+        public render() {
+            if (this.props.renderMode !== RenderMode.VIEW && this.state.loaded) {
+                const menuItems = this.state.menuItems.map(item => __(MenuItem, {
+                    key: item.key,
+                    value: item.key,
+                    primaryText: item.value,
+                }));
+                return _.span({ className: "metadata-field" }, __(SelectField, {
+                    fullWidth: true,
+                    hintText: "Select value",
+                    onChange: (evt: FormEvent<{}>, key: number) => {
+                        this.props.onChange(config.mapToModel(this.props.node, this.state.menuItems[key].key));
+                    },
+                    value: config.mapToView(this.props.node),
+                }, menuItems));
+            } else {
+                let value = config.mapToView(this.props.node);
+                if(this.state.loaded) {
+                    const item = this.state.menuItems.find(i => i.key === value);
+                    if(item) {
+                        value = item.value;
+                    }
+                }
+                return _.span({ className: "metadata-value" }, value);
+            }
+        }
+    }
+
+    return ListConstraintInner;
+};
+export default ListConstraint;
