62e9d3840639d18fe3b9a814a9011dddfe40344e
Lars Vierbergen <lars.vierbergen@xenit.eu>
2017-12-14 16:10:54 +0100

    [XENFIN-509] Implement multivalue textfields renderer

--- src/metadata/property/textbox.ts
+++ src/metadata/property/textbox.ts
@@ -1,31 +1,47 @@
 import TextField from "material-ui/TextField";
-import { Component, createElement as __, DOM as _, FormEvent, ReactElement } from "react";
+import { Component, createElement as __, DOM as _, FormEvent, ReactElement, ReactNode } from "react";
+
+declare var require: (m: string) => any;
+const ChipInput = require("material-ui-chip-input");
 
 import { FieldSkeleton_Props_t, RenderMode } from "../fields";
 import { PropertyRenderConfig_t, PropertyRenderer_t } from "./interface";
+import Label from "./label";
+
+type MultiValueTextBox_Props_t = {
+    hintText: ReactNode,
+    fullWidth: boolean,
+    onChange: (evt: FormEvent<{}>, v: string[]) => void,
+    value: string[],
+};
+
+function MultiValueTextBox(props: MultiValueTextBox_Props_t) {
+    return __(ChipInput, {
+        fullWidth: props.fullWidth,
+        hintText: props.hintText,
+        allowDuplicates: true,
+        onRequestAdd: (chip: string) => props.onChange(<FormEvent<{}>>{}, props.value.concat([chip])),
+        onRequestDelete: (chip: string, index: number) => props.onChange(<FormEvent<{}>>{}, props.value.filter((v, i) => i !== index)),
+        value: props.value,
+    });
+}
 
 const TextBox: PropertyRenderer_t<string | string[]> = (config: PropertyRenderConfig_t<string | string[]>) => {
+    const label = Label(config);
     // tslint:disable-next-line:only-arrow-functions
     return function TextBox(props: FieldSkeleton_Props_t) {
         const value = config.mapToView(props.node);
-        const isMultiValue = Array.isArray(value);
-        const stringValue = isMultiValue ? value.join(", ") : value;
         if (props.renderMode !== RenderMode.VIEW) {
-            if(!isMultiValue) {
-                return _.span({ className: "metadata-field" }, __(TextField, {
-                    fullWidth: true,
-                    hintText: "Type value...",
-                    onChange: (evt: FormEvent<{}>, v: string) => {
-                        props.onChange(config.mapToModel(props.node, v));
-                    },
-                    value: <string>value,
-                }));
-            } else {
-                // TODO: Implement handling of multivalue fields
-                return null;
-            }
+            return _.span({ className: "metadata-field" }, __(<any>(Array.isArray(value) ? MultiValueTextBox : TextField), {
+                fullWidth: true,
+                hintText: "Type value...",
+                onChange: (evt: FormEvent<{}>, v: string | string[]) => {
+                    props.onChange(config.mapToModel(props.node, v));
+                },
+                value,
+            }));
         } else {
-            return _.span({ className: "metadata-value" }, [stringValue]);
+            return __(label, props);
         }
     };
 };
