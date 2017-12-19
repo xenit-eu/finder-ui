3470bcb4edb4e8c43e7cc6a8de151456e89feb8c
Lars Vierbergen <lars.vierbergen@xenit.eu>
2017-12-19 16:06:55 +0100

    [XENFIN-550] Accept booleans and dates in the default property ColumnRenderer

--- src/doclist/renderer/property.ts
+++ src/doclist/renderer/property.ts
@@ -1,14 +1,32 @@
-import {DOM as _} from "react";
+import { DOM as _ } from "react";
 import { ColumnRenderer_Config_t, ColumnRenderer_Factory_t, ColumnRenderer_Props_t, ColumnRenderer_t } from "./interface";
 
-const PropertyRenderer: ColumnRenderer_Factory_t<string | string[]> = (config: ColumnRenderer_Config_t<string | string[]>): ColumnRenderer_t => {
+type Property_t = string | Date | boolean;
+
+const PropertyRenderer: ColumnRenderer_Factory_t<Property_t | Property_t[]> = (config: ColumnRenderer_Config_t<Property_t | Property_t[]>): ColumnRenderer_t => {
     // tslint:disable-next-line:only-arrow-functions
     return function Property(props: ColumnRenderer_Props_t) {
         let value = config.mapToView(props.node);
         if(!Array.isArray(value)) {
             value = [value];
         }
-        return _.span({}, value.join(", "));
+        return _.span({}, value.map(v => convertToString(config.parameters, v)).join(", "));
     };
 };
 export default PropertyRenderer;
+
+function convertToString(parameters: { [k: string]: any }, value: Property_t) {
+    if(typeof value === "string") {
+        return value;
+    }
+
+    if(typeof value === "boolean") {
+        return value ? "True" : "False";
+    }
+
+    if(value instanceof Date) {
+        return value.toLocaleDateString();
+    }
+
+    return value;
+}
