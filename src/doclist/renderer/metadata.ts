43ded29c349d1e7f8464326cf58b9a9d2799220d
Lars Vierbergen <lars.vierbergen@xenit.eu>
2017-12-19 17:31:05 +0100

    [XENFIN-550] Add renderers that wrap normal metadata renderers

---
+++ src/doclist/renderer/metadata.ts
@@ -0,0 +1,22 @@
+import { createElement as __, DOM as _ } from "react";
+
+import { PropertyRenderConfig_t, PropertyRenderer_t, PropertyRenderers, RenderMode } from "../../metadata";
+import { ColumnRenderer_Config_t, ColumnRenderer_Factory_t, ColumnRenderer_Props_t, ColumnRenderer_t } from "./interface";
+
+function wrapMetadataRenderer<T>(metadataRendererFactory: PropertyRenderer_t<T>): ColumnRenderer_Factory_t<T> {
+    return (config: ColumnRenderer_Config_t<T>): ColumnRenderer_t => {
+        const metadataRenderer = metadataRendererFactory(config as PropertyRenderConfig_t<T>);
+        return (props: ColumnRenderer_Props_t) => {
+            return __(metadataRenderer, {
+                node: props.node,
+                onChange: () => {},
+                renderMode: RenderMode.VIEW,
+            });
+        };
+    };
+}
+
+export const Currency = wrapMetadataRenderer(PropertyRenderers.Currency);
+export const Size = wrapMetadataRenderer(PropertyRenderers.Size);
+export const Mimetype = wrapMetadataRenderer(PropertyRenderers.Mimetype);
+export const ResourceResolver = wrapMetadataRenderer(PropertyRenderers.SelectBox);
