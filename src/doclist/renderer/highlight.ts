cdc179747c4a89fd3dc03cf35fe8a56e1a39613c
Robrecht Vanhuysse <robrecht.vanhuysse@xenit.eu>
2019-02-14 11:35:49 +0100

    INNOSEARCH-31 pseudo highlight renderer

--- src/doclist/renderer/highlight.ts
+++ src/doclist/renderer/highlight.ts
@@ -2,12 +2,35 @@ import { createElement as __, ReactNode } from "react";
 
 import { ColumnRenderer_Config_t, ColumnRenderer_Factory_t, ColumnRenderer_Props_t, ColumnRenderer_t } from "./interface";
 
-export type Highlight_t = string;
+/*export*/ type Highlight_t = string;
+
+
+//TODO Remove this class since its been replaced by one in finder-xenit
+//because fuck consistency?
 
 const HighlightRenderer: ColumnRenderer_Factory_t<Highlight_t[]> = (config: ColumnRenderer_Config_t<Highlight_t[]>): ColumnRenderer_t => {
     return function Highlight(props: ColumnRenderer_Props_t) {
-        return __("span", [], null);
+        let htmlGeneration: any[] = [];
+        props.highlights.forEach((highlightItem) => {
+            // One highlightInfo per snippet
+            highlightItem.snippets.forEach((snippet) => {
+                // Somehow the whole highlight shebang from finder-xenit needs to come in here,
+                // so we can have those juicy, juicy highlightInfos,
+                // which can then be poured into html.
+                
+                let highlightInfo/*:highlightInfo = parseHighlights(snippet)*/;
+                let highlightHtmlForNode = generateHtml(highlightInfo);
+                htmlGeneration.push(highlightHtmlForNode);
+            });
+        });
+        return __("span", [], htmlGeneration);
     };
 };
 
-export default HighlightRenderer;
+function generateHtml(highlightInfo: any/*highlightInfo*/): any/*Whatever React function*/{
+    return highlightInfo.map((pair:{text:string, highlighted:boolean}) => (
+        pair.highlighted ? __("bold", [], pair.text) : __("text", [], pair.text)
+    ));
+}
+
+//export default HighlightRenderer;
