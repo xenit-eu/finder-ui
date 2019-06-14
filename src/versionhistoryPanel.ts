8e34f326bea8cb1ef6bf610d5a19a65563aec945
Jasper Hilven <jasperhilven@gmail.com>
2019-06-14 14:08:58 +0200

    Remove unstable typing

--- src/versionhistoryPanel.ts
+++ src/versionhistoryPanel.ts
@@ -43,8 +43,8 @@ const avatarSvgStyle: CSSProperties = {
     fontSize: "18px",
     margin: "6px",
 };
-const avatar = _.div({ style: avatarStyle }, [
-    _.svg({ key: "svg", viewBox: "0 0 24 24", style: avatarSvgStyle }, [
+const avatar = _.div({ style: avatarStyle } as any, [
+    _.svg({ key: "svg", viewBox: "0 0 24 24", style: avatarSvgStyle as any }, [
         _.path({ key: "path", d: "M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" }),
     ]),
 ]);
