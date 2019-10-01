6aefc8c8280087b955fe923eda310a0577de122c
Zlatin Todorinski <todorinskiz@gmail.com>
2019-10-01 09:20:15 +0200

    Removed 'finder/' from relative path (#9)

--- src/docpreviewPdfjs.ts
+++ src/docpreviewPdfjs.ts
@@ -5,6 +5,6 @@ export function DocPreviewPdfJs({ src }: { src: string }): ReactElement<any> {
     if (!src || src.length === 0) {
         return _.div({ className: "PDFJS_Without_file" });
     }
-    const pdfJsWrapperSrc = "finder/resources/static/pdfjs/web/viewer.html?file=" + encodeURIComponent(src);
+    const pdfJsWrapperSrc = "resources/static/pdfjs/web/viewer.html?file=" + encodeURIComponent(src);
     return _.iframe({ className: "pdfjs-container-iframe", style: { border: "none" }, src: pdfJsWrapperSrc });
 }
