327ec9717ca1a60521ebfe0af10eab8c64f4c1e1
Lars Vierbergen <lars.vierbergen@xenit.eu>
2020-01-27 15:04:57 +0100

    [EFUU-28] Add unittest for RowMenu

---
+++ src/doclist/RowMenu.spec.ts
@@ -0,0 +1,94 @@
+import { configure, mount, shallow } from "enzyme";
+import * as Adapter from "enzyme-adapter-react-16";
+import * as jasmineEnzyme from "jasmine-enzyme";
+import { createElement } from "react";
+import { Fixture } from "../testUtils";
+import DynamicRowMenu, { RowMenu } from "./RowMenu";
+configure({ adapter: new Adapter() });
+
+describe("Doclist DynamicRowMenu", () => {
+    beforeEach(() => (<any>jasmineEnzyme)());
+
+    it("Renders row menu with passed parameters", () => {
+        const menuItems = [
+            {
+                key: "abc",
+                label: "xyz",
+                disabled: true,
+            },
+            {
+                key: "def",
+                label: "ghi",
+                disabled: false,
+            },
+        ];
+        const element = shallow(createElement(DynamicRowMenu, {
+            menuItems,
+            onMenuItemSelected: () => { },
+
+        }));
+
+        expect(element.find(RowMenu).length).toEqual(1);
+        expect(element.find(RowMenu).prop("menuItems")).toEqual(menuItems);
+        expect(element.find(RowMenu).prop("open")).toBe(false);
+
+    });
+
+    it("Renders the row menu and loads additional parameters when opened", async () => {
+        const menuItems = [
+            {
+                key: "abc",
+                label: "xyz",
+                disabled: true,
+            },
+            {
+                key: "def",
+                label: "ghi",
+                disabled: false,
+            },
+        ];
+        const promisesToAwait: Array<Promise<any>> = [];
+        const element = shallow(createElement(DynamicRowMenu, {
+            menuItems,
+            onMenuItemSelected: () => { },
+            onMenuLoadRequested: (callback) => {
+                const p: Promise<void> = new Promise((resolve) => {
+                    setTimeout(() => {
+                        callback(0, {
+                            key: "abc",
+                            label: "DDD",
+                            disabled: false,
+                        });
+                        resolve();
+                    }, 10);
+                });
+                promisesToAwait.push(p);
+                return p;
+            },
+        }));
+        // Trigger open of the menu
+        (element.find(RowMenu).prop("onRequestChange") as Function)(true);
+
+        // Menu should not be open yet
+        expect(element.find(RowMenu).prop("open")).toBe(false);
+
+        // Wait until promises have resolved
+        await Promise.all(promisesToAwait);
+
+        expect(element.find(RowMenu).prop("menuItems")).toEqual([
+            {
+                key: "abc",
+                label: "DDD",
+                disabled: false,
+            },
+            {
+                key: "def",
+                label: "ghi",
+                disabled: false,
+            },
+        ]);
+        expect(element.find(RowMenu).prop("open")).toBe(true);
+
+    });
+
+});
