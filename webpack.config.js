c9a59638ab6bf78b26e04fcdcc4b5106a8acd3a7
Lars Vierbergen <lars.vierbergen@xenit.eu>
2020-02-28 13:34:25 +0100

    [XENFIN-1125] Update webpack

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require("path");
+module.exports = {
+    resolve: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.json$/,
+                loader: 'json-loader'
+            },
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                options: {
+                    allowTsInNodeModules: true,
+                }
+            },
+            {
+                test: /\.less$/,
+                loader: "style-loader!css-loader!less-loader"
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+        ]
+    },
+    externals: { /// !!!! important !!!!
+        'cheerio': 'window',
+        'react/addons': true,
+        'react/lib/ExecutionEnvironment': true,
+        'react/lib/ReactContext': true
+    },
+};
