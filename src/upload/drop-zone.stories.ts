f781e655bba21fb150af3d848e4da57a1f564c0d
Lars Vierbergen <lars.vierbergen@xenit.eu>
2020-02-28 14:55:52 +0100

    [XENFIN-1125] Implement fake progress for upload dropzone

--- src/upload/drop-zone.stories.ts
+++
@@ -1,9 +0,0 @@
-import { createElement } from "react";
-import { DropZone } from "./drop-zone";
-
-export default {
-    title: "DropZone",
-    component: DropZone,
-};
-
-export const normal = () => createElement(DropZone, { process: () => { }, postProcessSelected: () => Promise.resolve(true) });
---
+++ src/upload/drop-zone.stories.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import { DropZone } from "./drop-zone";
+import { TestWrapper } from "../testUtils";
+import "./drop-zone.stories.less";
+
+export default {
+    title: "upload/drop-zone",
+    component: DropZone,
+};
+
+function fakeUploadProgress(files: File[], progress: (fileIdx: number, progress: number) => void, done: (fileIdx: number, id: string) => void) {
+    let currentFile = 0;
+    let currentFileProgress = 0;
+
+    let interval = setInterval(() => {
+        if (currentFileProgress < 100) {
+            currentFileProgress++;
+            progress(currentFile, (currentFileProgress / 100) * files[currentFile].size);
+        } else if (currentFile < files.length - 1) {
+            done(currentFile, "some-id");
+            currentFile++;
+            currentFileProgress = 0;
+        } else {
+            clearInterval(interval);
+        }
+    }, 100);
+}
+
+export const normal = () => <TestWrapper>
+    <div style={{ width: "calc(100vw - 100px)", height: "calc(100vh - 100px)" }}>
+        <DropZone disabled={false} process={fakeUploadProgress} postProcessSelected={() => Promise.resolve(true)}>
+            <div style={{ width: "100%", height: "100%", backgroundColor: "red", display: "table-cell" }} />
+        </DropZone>
+    </div>
+</TestWrapper>;
