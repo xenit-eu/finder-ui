a8383eee6f07f801adb80c4fa53e867bf3901ded
Lars Vierbergen <lars.vierbergen@xenit.eu>
2020-03-31 14:26:19 +0200

    [XENFIN-1146] Add PopupMenu and OverflowMenu components

---
+++ src/menu/PopupMenu.tsx
@@ -0,0 +1,34 @@
+import { IconButton, Menu } from "@material-ui/core";
+import * as React from "react";
+import { useTranslation } from "react-i18next";
+import { v4 } from "uuid";
+
+type PopupMenu_Props_t = {
+    icon: React.ReactElement,
+    children: (closeMenu: () => void) => React.ReactElement[],
+};
+export default function PopupMenu(props: PopupMenu_Props_t) {
+    const [menuId] = React.useState<string>(() => "menu-PopupMenu-" + v4());
+    const [anchorElement, setAnchorElement] = React.useState<HTMLElement | null>(null);
+    const open = !!anchorElement;
+    const { t } = useTranslation("finder-ui");
+    const onClose = () => setAnchorElement(null);
+    return <div>
+        <IconButton
+            aria-label={t("menu/PopupMenu/more")}
+            aria-owns={open ? menuId : undefined}
+            aria-haspopup="true"
+            onClick={(event: React.MouseEvent<HTMLElement>) => setAnchorElement(event.currentTarget)}
+            disabled={open}
+        >{props.icon}</IconButton>
+        <Menu
+            open={open}
+            anchorEl={anchorElement}
+            id={menuId}
+            onClose={onClose}
+        >
+            {props.children(onClose)}
+        </Menu>
+    </div>;
+
+}
