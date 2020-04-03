ff0df4f6a1af0eb741b4d34a2acc634a1f442ac8
Lars Vierbergen <lars.vierbergen@xenit.eu>
2020-04-03 12:20:49 +0200

    [XENFIN-1146] Add puppeteer tests for NewComment and EditableComment

--- src/comments/EditableComment.stories.tsx
+++ src/comments/EditableComment.stories.tsx
@@ -1,6 +1,8 @@
 import { action } from "@storybook/addon-actions";
 import { boolean } from "@storybook/addon-knobs";
+import { Page } from "puppeteer";
 import * as React from "react";
+import { interceptAction } from "../puppeteerActionInterceptor";
 import EditableComment from "./EditableComment";
 
 export default {
@@ -18,6 +20,37 @@ export const normal = () => <EditableComment
     onSave={action("save")}
     onCancel={action("cancel")}
 />;
+
+normal.story = {
+    parameters: {
+        async puppeteerTest(page: Page) {
+
+            const saveActionPromise = interceptAction(page, "save");
+            const cancelActionPromise = interceptAction(page, "cancel");
+
+            const buttons = await page.$$("button");
+
+            const buttonsText = await Promise.all(buttons.map((buttonHandle) => buttonHandle.evaluate((button: HTMLElement) => button.innerText)));
+
+            const saveButton = buttons[buttonsText.indexOf("COMMENTS/EDITABLECOMMENT/SAVE")];
+            expect(saveButton).toBeDefined();
+            const cancelButton = buttons[buttonsText.indexOf("COMMENTS/EDITABLECOMMENT/CANCEL")];
+            expect(cancelButton).toBeDefined();
+
+            const textArea = await page.$("[contenteditable]");
+            await textArea.type("Some comment text.");
+            await saveButton.click();
+            const saveAction = await saveActionPromise;
+            expect(saveAction.name).toEqual("save");
+            expect(saveAction.args).toEqual(["<p>Some comment text.Once a police officer caught Chuck Norris, the cop was lucky enough to escape with a warning.</p>"]);
+
+            await cancelButton.click();
+
+            const cancelAction = await cancelActionPromise;
+            expect(cancelAction.name).toEqual("cancel");
+        },
+    },
+};
 
 export const multiLine = () => <EditableComment
     comment={{
