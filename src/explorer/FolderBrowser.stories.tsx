8a2df2ba58920099dc7b5ae00205b945282ca48c
Lars Vierbergen <lars.vierbergen@xenit.eu>
2020-05-25 15:38:12 +0200

    [XENFIN-1178] Add interactive story for FolderBrowser

--- src/explorer/FolderBrowser.stories.tsx
+++ src/explorer/FolderBrowser.stories.tsx
@@ -1,5 +1,5 @@
 import { action } from "@storybook/addon-actions";
-import React from "react";
+import React, { useState } from "react";
 import FolderBrowser from "./FolderBrowser";
 import { ExplorerFolderState, ExplorerFolderType } from "./types";
 
@@ -31,19 +31,21 @@ const demoPath = [
     },
 ];
 
+const demoFolders = [
+    {
+        name: "2015-2016",
+        type: ExplorerFolderType.FOLDER,
+        state: ExplorerFolderState.CLOSED,
+    },
+    {
+        name: "2016-2017",
+        type: ExplorerFolderType.FOLDER,
+        state: ExplorerFolderState.CLOSED,
+    },
+];
+
 export const normal = () => <FolderBrowser path={demoPath}
-    folders={[
-        {
-            name: "2015-2016",
-            type: ExplorerFolderType.FOLDER,
-            state: ExplorerFolderState.CLOSED,
-        },
-        {
-            name: "2016-2017",
-            type: ExplorerFolderType.FOLDER,
-            state: ExplorerFolderState.LOADING,
-        },
-    ]}
+    folders={demoFolders}
     onOpen={action("open")}
     onSelect={action("select")}
 />;
@@ -72,3 +74,51 @@ export const empty = () => <FolderBrowser path={demoPath}
     onOpen={action("open")}
     onSelect={action("select")}
 />;
+
+function FolderBrowserInteractive() {
+    const [currentFolderIndex, setCurrentFolderIndex] = useState(2);
+    const [isEmptyFolder, setEmptyFolder] = useState(false);
+    const [selectedFolder, setSelectedFolder] = useState(null);
+    const [finalPath, setFinalPath] = useState([]);
+
+    let folders;
+
+    if (isEmptyFolder) {
+        folders = [];
+
+    } else if (currentFolderIndex >= demoPath.length) {
+        folders = demoFolders.map((f) => ({ ...f, selected: f.name === selectedFolder }));
+    } else {
+        folders = [{
+            ...demoPath[currentFolderIndex],
+            state: ExplorerFolderState.CLOSED,
+            selected: demoPath[currentFolderIndex].name === selectedFolder,
+        }];
+    }
+
+    return <FolderBrowser
+        path={demoPath.slice(0, currentFolderIndex).concat(finalPath)}
+        folders={folders}
+        onOpen={(folder) => {
+            const inDemoPath = demoPath.findIndex((f) => f.name === folder.name);
+
+            if (inDemoPath > -1) {
+                setCurrentFolderIndex(inDemoPath + 1);
+                setEmptyFolder(false);
+                setFinalPath([]);
+            } else {
+                setFinalPath([folder]);
+                setEmptyFolder(true);
+            }
+        }}
+        onSelect={(folder) => setSelectedFolder(folder.name)}
+    />;
+}
+
+export const interactive = () => <FolderBrowserInteractive />;
+
+interactive.story = {
+    parameters: {
+        storyshots: { disable: true },
+    },
+};
