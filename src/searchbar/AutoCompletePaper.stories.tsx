a783a4006dad4bcd1fabb8a90b8282bb32969b26
Lars Vierbergen <lars.vierbergen@xenit.eu>
2020-08-25 11:43:07 +0200

    [XENFIN-1241] Use material-ui TextField for consistency between browsers

--- src/searchbar/AutoCompletePaper.stories.tsx
+++ src/searchbar/AutoCompletePaper.stories.tsx
@@ -1,4 +1,4 @@
-import { Typography, withStyles } from "@material-ui/core";
+import { TextField, Typography, withStyles } from "@material-ui/core";
 import React, { useState } from "react";
 import AutocompletePaper from "./AutocompletePaper";
 
@@ -11,19 +11,19 @@ export default {
 const AutocompletePaperWithOutline = withStyles({ collapse: { outline: "3px dashed hotpink" } })(AutocompletePaper);
 
 export const closed = () => <div style={{ height: 500, outline: "1px dashed red" }}>
-    <AutocompletePaperWithOutline target={<input />} open={false}>
+    <AutocompletePaperWithOutline target={<TextField />} open={false}>
         <Typography variant="body2">Some contents...</Typography>
     </AutocompletePaperWithOutline>
 </div>;
 
 export const open = () => <div style={{ height: 500, outline: "1px dashed red" }}>
-    <AutocompletePaperWithOutline target={<input />} open={true}>
+    <AutocompletePaperWithOutline target={<TextField />} open={true}>
         <Typography variant="body2">Some contents...</Typography>
     </AutocompletePaperWithOutline>
 </div>;
 
 export const maxHeight = () => <div style={{ height: 500, outline: "1px dashed red" }}>
-    <AutocompletePaperWithOutline target={<input />} open={true}>
+    <AutocompletePaperWithOutline target={<TextField />} open={true}>
         <div>
             <div style={{ height: 900, width: 100, backgroundColor: "purple", marginLeft: 8 }}></div>
         </div>
@@ -33,7 +33,7 @@ export const maxHeight = () => <div style={{ height: 500, outline: "1px dashed r
 export const interactive = () => {
     const [value, setValue] = useState("");
     return <div style={{ height: 500, outline: "1px dashed red" }}>
-        <AutocompletePaperWithOutline target={<input value={value} onChange={(e) => setValue(e.target.value)} />} open={value.length > 0}>
+        <AutocompletePaperWithOutline target={<TextField value={value} onChange={(e) => setValue(e.target.value)} />} open={value.length > 0}>
             <Typography variant="body2">You have typed: {value}</Typography>
         </AutocompletePaperWithOutline>
     </div>;
