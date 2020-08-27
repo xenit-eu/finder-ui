b46e58362ac70c75ef37debdaa2752f4aad135e9
Lars Vierbergen <lars.vierbergen@xenit.eu>
2020-08-27 10:31:09 +0200

    [XENFIN-1242] Lift editing state out of EditableChip

--- src/searchbar/chips/EditableChip.stories.tsx
+++ src/searchbar/chips/EditableChip.stories.tsx
@@ -1,4 +1,3 @@
-import { TextField } from "@material-ui/core";
 import { action } from "@storybook/addon-actions";
 import React from "react";
 import TextComponent from "../renderer/Text";
@@ -27,10 +26,15 @@ const fieldWithNoData = {
     fieldValue: { start: null, end: null },
 };
 
-const editingProp = {
-    _editing,
-};
-const BaseEditableChip = (props) => <EditableChip viewComponent={TextComponent} editComponent={TextComponent} {...props} />;
+const BaseEditableChip = (props) => <EditableChip
+    viewComponent={TextComponent}
+    editComponent={TextComponent}
+    onBeginEditing={action("beginEditing")}
+    onCommitEditing={action("commitEditing")}
+    onCancelEditing={action("cancelEditing")}
+    editing={false}
+    {...props}
+/>;
 
 export const plain = () => <BaseEditableChip value={fieldWithValue} />;
 export const plainRange = () => <BaseEditableChip value={fieldWithRange} />;
@@ -39,8 +43,8 @@ export const withDeleteRange = () => <BaseEditableChip value={fieldWithRange} on
 export const withChange = () => <BaseEditableChip value={fieldWithValue} onDelete={action("delete")} onChange={action("change")} />;
 export const withChangeRange = () => <BaseEditableChip value={fieldWithRange} onDelete={action("delete")} onChange={action("change")} />;
 export const withChangeRangeOpenEnded = () => <BaseEditableChip value={fieldWithOpenRange} onDelete={action("delete")} onChange={action("change")} />;
-export const withChangeEditing = () => <BaseEditableChip value={fieldWithValue} onDelete={action("delete")} onChange={action("change")} {...editingProp} />;
-export const withChangeEditingRange = () => <BaseEditableChip value={fieldWithRange} onDelete={action("delete")} onChange={action("change")} {...editingProp} />;
+export const withChangeEditing = () => <BaseEditableChip value={fieldWithValue} onDelete={action("delete")} onChange={action("change")} editing />;
+export const withChangeEditingRange = () => <BaseEditableChip value={fieldWithRange} onDelete={action("delete")} onChange={action("change")} editing />;
 
-export const withChangeEditingRangeOpenEnded = () => <BaseEditableChip value={fieldWithOpenRange} onDelete={action("delete")} onChange={action("change")} {...editingProp} />;
-export const withChangeEditingRangeError = () => <BaseEditableChip value={fieldWithNoData} onDelete={action("delete")} onChange={action("change")} {...editingProp} />;
+export const withChangeEditingRangeOpenEnded = () => <BaseEditableChip value={fieldWithOpenRange} onDelete={action("delete")} onChange={action("change")} editing />;
+export const withChangeEditingRangeError = () => <BaseEditableChip value={fieldWithNoData} onDelete={action("delete")} onChange={action("change")} editing />;
--- src/searchbar/chips/EditableChip.tsx
+++ src/searchbar/chips/EditableChip.tsx
@@ -3,17 +3,13 @@ import { emphasize } from "@material-ui/core/styles/colorManipulator";
 import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 import CloseIcon from "@material-ui/icons/Close";
 import classnames from "classnames";
-import React, { KeyboardEvent, useCallback, useState } from "react";
+import React, { KeyboardEvent } from "react";
 import { useTranslation } from "react-i18next";
 import invariant from "tiny-invariant";
 import FieldRenderer, {FieldRendererComponent, ISearchboxFieldData, isEmptyValue, isRange} from "../FieldRenderer";
 import ChipIconButton from "./ChipIconButton";
 import ResizableChip from "./ResizableChip";
 import useKeypressHandler from "./useKeypressHandler";
-
-// @internal
-// tslint:disable-next-line
-export const _editing = Symbol();
 
 export type EditableChip_Props_t<T, D extends ISearchboxFieldData<T>> = {
     /**
@@ -26,10 +22,7 @@ export type EditableChip_Props_t<T, D extends ISearchboxFieldData<T>> = {
      */
     readonly onDelete?: () => void,
     /**
-     * Function that is called when the chip has been edited.
-     * If this function is not present, the chip will not be editable.
-     *
-     * If this function is present, {@link editComponent} must be present as well.
+     * Function that is called when the chip data is changed
      */
     readonly onChange?: (value: D) => void,
     /**
@@ -39,12 +32,32 @@ export type EditableChip_Props_t<T, D extends ISearchboxFieldData<T>> = {
     /**
      * Component that will be used to display field values when the chip is in edit-mode
      */
-    readonly editComponent?: FieldRendererComponent<T, EditableChip_ChangeComponent_Props_t<T>>,
-    // @internal for storybook testing only
-    readonly _editing?: typeof _editing,
+    readonly editComponent?: FieldRendererComponent<T, EditableChip_ChangeComponent_Props_t>,
+
+    /**
+     * Component is in edit mode
+     *
+     * If this is true, {@link onChange} and {@link editComponent} must be present as well.
+     */
+    readonly editing: boolean,
+
+    /**
+     * Function that is called when editing is initiated
+     */
+    readonly onBeginEditing?: () => void,
+
+    /**
+     * Function that is called when editing is cancelled
+     */
+    readonly onCancelEditing?: () => void,
+
+    /**
+     * Function that is called when editing is committed
+     */
+    readonly onCommitEditing?: () => void,
 };
 
-type EditableChip_ChangeComponent_Props_t<T> = {
+type EditableChip_ChangeComponent_Props_t = {
     onKeyUp: (event: KeyboardEvent) => void,
 };
 
@@ -62,49 +75,36 @@ const editableChipStyles = (theme: Theme) => ({
 });
 
 function EditableChip<T, D extends ISearchboxFieldData<T>>(props: EditableChip_Props_t<T, D> & WithStyles<typeof editableChipStyles>) {
-    const [isEditing, setEditing] = useState(props._editing ? true : false);
-    const [value, setValue] = useState<D|null>(null);
-    const cancelEditing = useCallback(() => {
-        setValue(null);
-        setEditing(false);
-    }, [setValue, setEditing]);
-    const commitEditing = useCallback(() => {
-        const finalValue = value ?? props.value;
-        if (!isInvalid(finalValue)) {
-            props.onChange!(finalValue);
-            cancelEditing();
-        }
-    }, [props.onChange, value, props.value, cancelEditing]);
 
-    if (props.onChange) {
-        invariant(props.editComponent, "editComponent is required when onChange is set.");
+    if (props.editing) {
+        invariant(props.editComponent, "editComponent is required when editing is set.");
+        invariant(props.onChange, "onChange is required when editing is set.");
     }
-    if (props._editing) {
-        invariant(props._editing === _editing, "_editing is internal, only to be used in storybook.");
 
-    }
     invariant(!isEmptyValue(props.value.fieldValue), "value.fieldValue can not be an empty value.");
 
-    const keyUp = useKeypressHandler({
-        onExit: () => isEditing && cancelEditing(),
-        onCommit: () => isEditing && commitEditing(),
-        onModify: () => !isEditing && props.onChange && setEditing(true),
-    });
+    const handlers = {
+        onExit: props.editing ? props.onCancelEditing : undefined,
+        onCommit: props.editing ? (isInvalid(props.value) ? () => { } : props.onCommitEditing) : undefined,
+        onModify: !props.editing ? props.onBeginEditing : undefined,
+    };
+
+    const keyUp = useKeypressHandler(handlers);
 
     return <ResizableChip
-        onDoubleClick={props.onChange && !isEditing ? () => setEditing(true) : undefined}
-        onDelete={isEditing || !props.onDelete ? undefined : () => props.onDelete!()}
+        onDoubleClick={handlers.onModify}
+        onDelete={props.editing ? undefined : props.onDelete}
         className={classnames({
-            [props.classes.invalidData]: isInvalid(value ?? props.value),
+            [props.classes.invalidData]: isInvalid(props.value),
         })}
         label={<EditModeChipComponent<T, D>
-            value={value ?? props.value}
-            onChange={setValue}
-            isEditing={isEditing}
+            value={props.value}
+            onChange={props.onChange!}
+            isEditing={props.editing}
             viewComponent={props.viewComponent}
             editComponent={props.editComponent!}
-            onCommit={commitEditing}
-            onCancel={cancelEditing}
+            onCommit={handlers.onCommit}
+            onCancel={handlers.onExit}
         />}
         onKeyUp={keyUp}
     />;
@@ -115,8 +115,8 @@ export default withStyles(editableChipStyles)(EditableChip);
 type EditModeChipComponent_Props_t<T, D extends ISearchboxFieldData<T>> = {
     readonly value: D,
     readonly onChange: (value: D) => void,
-    readonly onCancel: () => void,
-    readonly onCommit: () => void,
+    readonly onCancel?: () => void,
+    readonly onCommit?: () => void,
     readonly viewComponent: FieldRendererComponent<T, {}>,
     readonly editComponent: FieldRendererComponent<T, EditableChip_ChangeComponent_Props_t<T>>,
     readonly isEditing: boolean;
@@ -138,12 +138,12 @@ function EditModeChipComponent<T, D extends ISearchboxFieldData<T>>(props: EditM
                 component={props.editComponent}
                 componentProps={{ onKeyUp }}
             />
-            <ChipIconButton onClick={props.onCommit} color={isInvalid(props.value) ? "inherit" : "primary"} disabled={isInvalid(props.value)}>
+            {props.onCommit && <ChipIconButton onClick={() => props.onCommit!()} color={isInvalid(props.value) ? "inherit" : "primary"} disabled={isInvalid(props.value)}>
                 <CheckCircleIcon aria-label={t("searchbar/chips/EditableChip/edit-done")} />
-            </ChipIconButton>
-            <ChipIconButton onClick={props.onCancel} color="inherit">
+            </ChipIconButton>}
+            {props.onCancel && <ChipIconButton onClick={() => props.onCancel!()} color="inherit">
                 <CloseIcon aria-label={t("searchbar/chips/EditableChip/edit-cancel")} />
-            </ChipIconButton>
+            </ChipIconButton>}
         </>;
     }
 }
